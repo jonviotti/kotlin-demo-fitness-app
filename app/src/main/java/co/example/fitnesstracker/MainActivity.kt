3ddd0f3cd9bb06bac776daeac476e88a993f3252
jviotti <jonviotti@gmail.com>
2025-10-20 17:42:18 -0300

    first commit of fitness application

---
+++ app/src/main/java/co/example/fitnesstracker/MainActivity.kt
@@ -0,0 +1,24 @@
+package co.example.fitnesstracker
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.LinearLayout
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var btn_imc:LinearLayout
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btn_imc = findViewById(R.id.btn_imc)
+
+        btn_imc.setOnClickListener {
+            //navegar para prox tela
+            val i = Intent(this, ImcActivity::class.java) // codigo padrao para abrir uma segunda activity
+            startActivity(i)
+        }
+    }
+}
